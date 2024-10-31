59eeea56f14523de83962fa32674fb793a2cbc1e
Emma Brostrom <emma.brostrom@canonical.com>
2024-10-31 12:16:03 +0100

    HelloWorld endpoint

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func HelloWorld(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Hello, World!")
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/hello", HelloWorld)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
