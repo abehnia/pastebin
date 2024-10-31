7d9c1680b9665fdc03ad0c964553da253f4f4325
Themistoklis Spanoudis <themistoklis.spanoudis@canonical.com>
2024-10-31 15:24:05 +0100

    Remove comments

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 	"time"
 
 	"database/sql"
+
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
 )
 
-// Bin represents a text bin with text content.
 type BinRequest struct {
 	Text  string `json:"text"`
 	Title string `json:"title"`
@@ -27,19 +27,15 @@ type Bin struct {
 }
 
 func main() {
-	// Initialize the router.
 	r := mux.NewRouter()
 
-	// Define the routes.
 	r.HandleFunc("/bins", createBinHandler).Methods("POST")
 	r.HandleFunc("/bins/{uuid}", getBinHandler).Methods("GET")
 
-	// Start the HTTP server.
 	log.Println("Server started on :8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-// createBinHandler handles the creation of a new bin.
 func createBinHandler(w http.ResponseWriter, r *http.Request) {
 	var newBin BinRequest
 
@@ -51,14 +47,12 @@ func createBinHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	// Parse the request body.
 	err = json.NewDecoder(r.Body).Decode(&newBin)
 	if err != nil {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
 
-	// Generate a new UUID for the bin.
 	id := uuid.New().String()
 
 	_, err = db.Exec("INSERT INTO pastebin(id, timestamp, title, content) VALUES($1, $2, $3, $4)", id, time.Now().UTC().Unix(), newBin.Title, newBin.Text)
@@ -67,14 +61,11 @@ func createBinHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Respond with the UUID.
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]string{"id": id})
 }
 
-// getBinHandler retrieves a bin by UUID.
 func getBinHandler(w http.ResponseWriter, r *http.Request) {
-	// Get the UUID from the URL path.
 	vars := mux.Vars(r)
 	id := vars["uuid"]
 
@@ -95,7 +86,6 @@ func getBinHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Println(bin)
-	// Respond with the bin's text.
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(bin)
 }
